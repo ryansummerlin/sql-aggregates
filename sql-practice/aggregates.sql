0e7b4bcd12dc8dc33b0a1d882bc2e61d0abb00fd
ryansummerlin <ryansummerlin777@gmail.com>
2023-05-19 13:44:39 -0700

    finished the bonus phase

--- sql-practice/aggregates.sql
+++ sql-practice/aggregates.sql
@@ -9,7 +9,19 @@ SELECT name, MIN(birth_year) as oldest_birth_year FROM cats;
 
 SELECT name, MAX(birth_year) as youngest_birth_year FROM cats;
 
--- Both at once
 
-SELECT name, birth_year FROM cats
-WHERE MIN(birth_year) OR MAX(birth_year);
+-- Bonus Step 1
+
+SELECT cats.name, COUNT(toys.id) AS num_toys FROM cats
+    JOIN toys ON (cats.id = toys.cat_id)
+    GROUP BY cats.name
+    ORDER BY num_toys DESC;
+
+
+-- Bonus Step 2
+
+SELECT cats.name, COUNT(toys.id) AS num_toys FROM cats
+    JOIN toys ON (cats.id = toys.cat_id)
+    GROUP BY cats.name
+    HAVING num_toys >= 2
+    ORDER BY num_toys DESC;
